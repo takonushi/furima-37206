6fa6143181e25a7235b7469b830008fd8b07df8e
takonushi <hewitt0120@gmail.com>
2022-01-18 17:43:05 +0900

    #ユーザー新規登録機能の実装
    ・バリデーションの記述追加
    ・ログイン状態に応じてヘッダーの表示が変更されるように実装(※ログインユーザーの詳細は#のまま)
    ・ユーザー登録に対してストロングパラメーターの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  validates :first_name_reading, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :last_name_reading, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birth_date, presence: true
+  validates :nickname, presence: true
+
 end
