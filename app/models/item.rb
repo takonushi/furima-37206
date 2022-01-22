6e49b92cad44c86283d87d72200076d2df3f3977
takonushi <hewitt0120@gmail.com>
2022-01-23 00:42:50 +0900

    ActiveStorage実装、アソシエーション・バリデーション設定
    ・ActiveStorage用Gemのインストールとテーブル作成
    ・userモデルとitemモデルのアソシエーションを定義
    ・itemモデルへバリデーションの設定
    ・views/items配下にnew.html.erbファイルを設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :charge_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :number_of_days_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :price, presence: true
+
 end
