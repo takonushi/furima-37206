21ebdc0b90e0d66b8b82f7a2feb83e19e0904461
takonushi <hewitt0120@gmail.com>
2022-01-29 08:59:08 +0900

    商品削除機能の実装
    ・destory用ルーティングの定義
    ・destroyアクションの定義
    ・show.html.erb編集
    ・Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :delete]
   before_action :set_item, only: [:show, :edit, :update]
-  
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
