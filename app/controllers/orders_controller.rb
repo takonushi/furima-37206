4a0072e4538dbf0a18f973bef6b42bc10f2d7e35
takonushi <hewitt0120@gmail.com>
2022-02-02 16:15:33 +0900

    Formオブジェクト実装前の暫定実装
    ・orderテーブルとsendingテーブルそれぞれにデータが書き込まれるように修正
    　(Formオブジェクト実装前のため、暫定的な設定)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     if @order.valid?
       pay_item
       @order.save
+      Sending.create(sending_params)
       return redirect_to root_path
     else
       render 'index'
@@ -21,6 +22,10 @@ class OrdersController < ApplicationController
     #記載だと「param is missing or the value is empty:order」というエラーになる。
     #別途、確認が必要。
     params.permit(:item_id).merge(user_id: current_user.id, token: params[:token], price: params[:price])
+  end
+
+  def sending_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(order_id: @order.id)
   end
 
   def pay_item
--- app/models/sending.rb
+++ app/models/sending.rb
@@ -1,3 +1,11 @@
 class Sending < ApplicationRecord
   belongs_to :order
+
+
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
 end
