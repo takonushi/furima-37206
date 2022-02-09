297358e41ef42b3c2077b19f2ed07779ce34929e
takonushi <hewitt0120@gmail.com>
2022-02-09 09:49:02 +0900

    不要な記述の削除

--- app/models/order_sending.rb
+++ app/models/order_sending.rb
@@ -1,7 +1,7 @@
 class OrderSending
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id, :user_id,
-                :item_id, :token, :price
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :user_id,
+                :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
